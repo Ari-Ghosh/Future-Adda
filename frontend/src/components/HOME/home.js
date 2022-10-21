8c29254d77dea95fbade260624d75ba927c2921f
web-dev-champ <110686082+web-dev-champ@users.noreply.github.com>
2022-10-21 23:18:16 +0530

    Update home.js

--- frontend/src/components/HOME/home.js
+++ frontend/src/components/HOME/home.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import Navigation from './Navigation';
+import Feat from "./Feat";
+import Inst from "./Inst";
+import Comp from "./Comp";
+import Busi from "./Busi";
+import Transform from "./Transform";
+
 
 
 const Home = () => {
     return (
-       
+     <div>  
       <Navigation />
-     
+      <Feat />
+      <Inst />
+      <Comp />
+      <Busi />
+      <Transform />
+     </div>
     );
 }
 
